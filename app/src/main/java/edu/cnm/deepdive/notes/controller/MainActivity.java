47176049e7bc5eafb848a1a28faf80e8105c739f
nick-bennett <nbennett10@cnm.edu>
2025-06-10 16:16:24 -0600

    Start entity classes.

---
+++ app/src/main/java/edu/cnm/deepdive/notes/controller/MainActivity.java
@@ -0,0 +1,29 @@
+package edu.cnm.deepdive.notes.controller;
+
+import android.os.Bundle;
+
+import android.util.Log;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import dagger.hilt.android.AndroidEntryPoint;
+import edu.cnm.deepdive.notes.databinding.ActivityMainBinding;
+import org.jetbrains.annotations.NotNull;
+
+@AndroidEntryPoint
+public class MainActivity extends AppCompatActivity {
+
+  private static final String TAG = MainActivity.class.getSimpleName();
+
+  private ActivityMainBinding binding;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    binding = ActivityMainBinding.inflate(getLayoutInflater());
+    setContentView(binding.getRoot());
+  }
+
+
+
+}
