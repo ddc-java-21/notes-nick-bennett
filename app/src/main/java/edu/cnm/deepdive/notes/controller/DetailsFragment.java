8cb2b034c9963446db1e5567dd65258757b4a71a
nick-bennett <nbennett10@cnm.edu>
2025-06-18 09:34:49 -0600

    Capture workflow in process.

--- app/src/main/java/edu/cnm/deepdive/notes/controller/DetailsFragment.java
+++ app/src/main/java/edu/cnm/deepdive/notes/controller/DetailsFragment.java
@@ -2,27 +2,38 @@ package edu.cnm.deepdive.notes.controller;
 
 import android.Manifest;
 import android.Manifest.permission;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.activity.result.contract.ActivityResultContracts.TakePicture;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.ViewModelProvider;
 import com.google.android.material.snackbar.Snackbar;
 import edu.cnm.deepdive.notes.R;
 import edu.cnm.deepdive.notes.databinding.FragmentDetailsBinding;
+import edu.cnm.deepdive.notes.model.entity.Image;
 import edu.cnm.deepdive.notes.model.pojo.NoteWithImages;
+import edu.cnm.deepdive.notes.service.ImageFileProvider;
 import edu.cnm.deepdive.notes.viewmodel.NoteViewModel;
+import java.io.File;
+import java.util.UUID;
 
 public class DetailsFragment extends Fragment {
+
+  private static final String TAG = DetailsFragment.class.getSimpleName();
+  private static final String AUTHORITY = ImageFileProvider.class.getName().toLowerCase();
 
   private final ActivityResultLauncher<String> requestCameraPermissionLauncher =
       registerForActivityResult(new ActivityResultContracts.RequestPermission(), (granted) -> {
@@ -37,6 +48,7 @@ public class DetailsFragment extends Fragment {
   private NoteViewModel viewModel;
   private long noteId;
   private NoteWithImages note;
+  private ActivityResultLauncher<Uri> takePictureLauncher;
 
   @Override
   public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -69,6 +81,14 @@ public class DetailsFragment extends Fragment {
     } else {
       note = new NoteWithImages();
     }
+    viewModel
+        .getCaptureUri()
+        .observe(owner, (uri) -> {
+          Image image = new Image();
+          image.setUri(uri);
+          note.getImages().add(image);
+        });
+    takePictureLauncher = registerForActivityResult(new TakePicture(), viewModel::confirmCapture);
     checkCameraPermission();
   }
 
@@ -106,9 +126,23 @@ public class DetailsFragment extends Fragment {
 
   private void explainCameraPermission() {
     Snackbar.make(
-        binding.getRoot(), R.string.camera_permission_explanation, Snackbar.LENGTH_INDEFINITE)
+            binding.getRoot(), R.string.camera_permission_explanation, Snackbar.LENGTH_INDEFINITE)
         .setAction(android.R.string.ok, (v) -> requestCameraPermission())
         .show();
+  }
+
+  private void capture() {
+    Context context = requireContext();
+    File captureDir = new File(context.getFilesDir(), getString(R.string.capture_directory));
+    //noinspection ResultOfMethodCallIgnored
+    captureDir.mkdir();
+    File captureFile;
+    do {
+      captureFile = new File(captureDir, UUID.randomUUID().toString());
+    } while (captureFile.exists());
+    Uri uri = FileProvider.getUriForFile(context, AUTHORITY, captureFile);
+    viewModel.setPendingCaptureUri(uri);
+    takePictureLauncher.launch(uri);
   }
 
 }
--- app/src/main/java/edu/cnm/deepdive/notes/viewmodel/NoteViewModel.java
+++ app/src/main/java/edu/cnm/deepdive/notes/viewmodel/NoteViewModel.java
@@ -1,6 +1,7 @@
 package edu.cnm.deepdive.notes.viewmodel;
 
 import android.content.Context;
+import android.net.Uri;
 import android.util.Log;
 import androidx.annotation.NonNull;
 import androidx.lifecycle.DefaultLifecycleObserver;
@@ -25,8 +26,11 @@ public class NoteViewModel extends ViewModel implements DefaultLifecycleObserver
   private final NoteRepository repository;
   private final MutableLiveData<Long> noteId;
   private final LiveData<NoteWithImages> note;
+  private final MutableLiveData<Uri> captureUri;
   private final MutableLiveData<Throwable> throwable;
   private final CompositeDisposable pending;
+
+  private Uri pendingCaptureUri;
 
   @Inject
   NoteViewModel(@ApplicationContext Context context, NoteRepository repository) {
@@ -34,6 +38,7 @@ public class NoteViewModel extends ViewModel implements DefaultLifecycleObserver
     this.repository = repository;
     noteId = new MutableLiveData<>();
     note = Transformations.switchMap(noteId, repository::get);
+    captureUri = new MutableLiveData<>();
     throwable = new MutableLiveData<>();
     pending = new CompositeDisposable();
   }
@@ -52,6 +57,19 @@ public class NoteViewModel extends ViewModel implements DefaultLifecycleObserver
 
   public LiveData<NoteWithImages> getNote() {
     return note;
+  }
+
+  public LiveData<Uri> getCaptureUri() {
+    return captureUri;
+  }
+
+  public void setPendingCaptureUri(Uri pendingCaptureUri) {
+    this.pendingCaptureUri = pendingCaptureUri;
+  }
+
+  public void confirmCapture(boolean success) {
+    captureUri.setValue(success ? pendingCaptureUri : null);
+    pendingCaptureUri = null;
   }
 
   public void save(NoteWithImages note) {
